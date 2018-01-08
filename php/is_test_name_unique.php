bde43e2599c59018d1ea3e4cac2ef06e7124f469
rsubramonian <rsubramonian@nerdwallet.com>
2018-01-08 13:04:55 -0800

    ok

--- php/is_test_name_unique.php
+++ php/is_test_name_unique.php
@@ -6,8 +6,9 @@ function is_test_name_unique(
 {
   $test_type_id = lkp("test_type", $test_type);
   $archived_id  = lkp("state", "archived");
-  $where_clause  = "state_id != $archived_id and ";
-  $where_clause .= "test_type_id = $test_type_id ";
+  $where_clause  = " name = '$test_name' and ";
+  $where_clause .= " state_id != $archived_id and ";
+  $where_clause .= " test_type_id = $test_type_id ";
   $R = db_get_rows("test", $where_clause);
   if ( is_null($R)) {
     return true;
