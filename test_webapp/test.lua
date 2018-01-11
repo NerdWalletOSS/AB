87009a6582bb920d4f3e306d6b9044db4fa8efd1
rsubramonian <rsubramonian@nerdwallet.com>
2018-01-10 19:30:14 -0800

    ok

---
+++ test_webapp/test.lua
@@ -0,0 +1,41 @@
+
+JSON = (loadfile "JSON.lua")() -- one-time load of the routines
+ T = {}
+ T.abc = { hello  = "world" }
+ T.def = { hello2  = "world2" }
+
+local cURL = require "cURL"
+local ltn12 = require("ltn12")
+
+function trim1(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+--============================
+hdrs = {}
+local function snarf_headers (str)
+  hdrs[#hdrs+1] = trim1(str)
+end
+--============================
+body = ""
+local function snarf_body (str)
+  body = str
+end
+
+c = cURL.easy{
+  url        = "173.255.252.157:8080/_foo.php?gkey=gval",
+  post       = true,
+
+  postfields = JSON:encode(T),
+  httpheader = {
+    "Content-Type: application/text";
+  };
+  writefunction  = snarf_body,
+  headerfunction = snarf_headers,
+}
+
+x = c:perform()
+print("Printing headers")
+for k, v in ipairs(hdrs) do print(k, v) end
+print("Printing body")
+print(body)
+print("ALL DONE")
