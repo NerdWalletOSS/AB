5edebef8dd3935c8a1abcb7f3ba810df83d39a14
i <i@ljabn.east1.stage.nerdwallet.biz>
2018-01-25 21:02:54 -0500

    adding aio

--- aio.sh
+++ aio.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+set-env -e
+sudo apt-get install gcc python python-pip -y
+sudo pip install pystatsd
+rm -rf bin
+rm -f bin.tar.gz
+mkdir bin
+cd src
+make clean
+cd ../curl-7.51.0/
+make clean
+./configure
+make
+cd ../src
+make
+cp ab_httpd ../bin
+cd ../
+mkdir ./bin/libs
+find ./ -name "*.so*" -exec cp {} ./bin/libs \;
+find ./src -name "*.lua" -exec cp {} ./bin \;
+tar -cvzf bin.tar.gz ./bin
 
-echo "Place holder for deployment stuff"
