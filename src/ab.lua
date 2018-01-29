3fe687de8e9a2f0cbcb4d9ce803e516ef30fa56f
i <i@ljabn.east1.stage.nerdwallet.biz>
2018-01-29 14:08:39 -0500

    adding add test and aio

--- src/ab.lua
+++ src/ab.lua
@@ -1,2 +1,9 @@
 local csv = require "csv"
 local json = require("json")
+local ffi = require 'ffi' 
+function add(c_str, c_data)
+   -- print("hi from lua", c_str)
+   local x = ffi.cast("int*", c_data)
+   print(c_str)
+   x[0] = 9
+end
--- src/add_test.c
+++ src/add_test.c
@@ -3,33 +3,29 @@
 #include "ab_globals.h"
 #include "add_test.h"
 
-int 
+int cdata[1];
+  int
 l_add_test(
     const char *args
     )
 {
-  int * cdata = malloc(sizeof(int));
   cdata[0]=5;
-  printf("original value: %d\n", cdata[0]);
+  // printf("original value: %d\n", cdata[0]);
   int status = 0;
-  status = luaL_dostring(g_L, "return require 'add'");
-  if (status != 0) {
-    fprintf(stderr, "Running require failed: %s\n", lua_tostring(g_L,-1));
+  lua_getglobal(g_L, "add");
+  if ( !lua_isfunction(g_L, -1)) {
+    fprintf(stderr, "Function add does not exist in lua's global space\n");
     lua_pop(g_L, 1);
-  } else if ( !lua_isfunction(g_L, -1)) {
-    fprintf(stderr, "Running require did not return a function \n");
-    lua_pop(g_L, 1);
+    go_BYE(-1);
   }
-  cBYE(status); 
-  lua_pushstring(g_L, args);
+  lua_pushstring(g_L, args); // not pushing string as it causes a copy
   lua_pushlightuserdata(g_L, cdata);
   status = lua_pcall(g_L, 2, 0, 0);
   if (status != 0) {
     fprintf(stderr, "calling function add failed: %s\n", lua_tostring(g_L, -1));
     lua_pop(g_L, 1);
   }
-  printf("changed value: %d\n", cdata[0]);
+  // printf("changed value: %d\n", cdata[0]);
 BYE:
-  free(cdata);
   return status;
 }
