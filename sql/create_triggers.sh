57e3db3ed11deb935a41cec3ec871b517cfaefa2
i <i@ljabn.east1.stage.nerdwallet.biz>
2018-01-29 20:06:51 -0500

    fixing triggers

--- sql/create_triggers.sh
+++ sql/create_triggers.sh
@@ -32,6 +32,7 @@ do
         print $0;
         print "log_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
         print "log_ime_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+        print "modification_type CHAR(8),"
       } else{
         print $0;
       }
@@ -46,26 +47,38 @@ do
     }
       my_str = "";
       count = 1;
-      for(i=1; i<NF && count<=2; i++) {
+      for(i=1; i<=NF && count<=2; i++) {
         if ($i != "") {
         my_str = my_str" "$i;
         count = count + 1;
       }
       }
-      my_str = my_str",";
+      if (NR != f_length -1) {
+        my_str = my_str",";
+      }
       print my_str;
     }
   }
   END{
     print "delimiter |"
-    print "CREATE TRIGGER " name"_trigger"
-    print "\tBEFORE INSERT OR UPDATE ON "name
+    print "CREATE TRIGGER " name"_update_trigger"
+    print "\tBEFORE UPDATE ON "name
     print "\tFOR EACH ROW"
     print "\tBEGIN"
-    print "\t\tINSERT INTO log_" name" (" fields") VALUES(" new_fields ");"
+    print "\t\tINSERT INTO log_" name" (modification_type, " fields") VALUES(\"UPDATE\", " new_fields ");"
+    print "\tEND;"
+    print "|"
+    print "delimiter ;"
+    print ""
+    print "delimiter |"
+    print "CREATE TRIGGER " name"_insert_trigger"
+    print "\tBEFORE INSERT ON "name
+    print "\tFOR EACH ROW"
+    print "\tBEGIN"
+    print "\t\tINSERT INTO log_" name" (modification_type, " fields") VALUES(\"INSERT\", " new_fields ");"
     print "\tEND;"
     print "|"
     print "delimiter ;"
 
-  }'
+  }' | sed 's/,,/,/g'
 done
