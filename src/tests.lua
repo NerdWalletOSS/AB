687950ea04c5d1083fb832817932b57c7f8532a0
Indrajeet Singh <isingh@nerdwallet.com>
2018-02-05 22:38:42 -0500

    adding state check

--- src/tests.lua
+++ src/tests.lua
@@ -1,3 +1,4 @@
+-- TODO assert cleanups is important
 local dbg = require 'debugger'
 local json = require 'json'
 local assertx = require 'assertx'
@@ -7,6 +8,24 @@ local consts = require 'ab_consts'
 local assertx = require 'assertx'
 local g_seed1 = 961748941 -- TODO remove manual copy
 local spooky_hash = require 'spooky_hash'
+
+local function create_state_table(consts)
+  local states = {}
+  for k,v in pairs(consts) do
+    local val = nil
+    for match in k:gmatch("TEST_STATE_(%w+)") do
+      val = match
+    end
+    if val ~= nil then
+      states[val:lower()] = v
+      -- print(val:lower(), v)
+    end
+  end
+  return states
+end
+
+local states = create_state_table(consts)
+
 local function add_variants(c_test, json_table)
   local variants = assert(json_table.Variants, "Test should have variants")
   assert(type(variants) == "table", "Variants should be an array of variants")
@@ -34,30 +53,51 @@ local function add_variants(c_test, json_table)
   assertx(total == 100, "all the percentages should add up to 100, added to ", total)
 end
 
-local function get_test(g_tests, test_name)
+local function get_test(g_tests, test_name, c_index)
   local name_hash = spooky_hash.spooky_hash64(test_name, #test_name, g_seed1)
   local position = name_hash % consts.AB_MAX_NUM_TESTS
+  local original_position = position
+  -- c_index = ffi.cast("int*", c_index)
   g_tests = ffi.cast("TEST_META_TYPE*", g_tests)
   local test = nil
   local stop = false
   -- dbg()
-  repeat 
-    test = ffi.cast("TEST_META_TYPE*", g_tests) + position
+  repeat
+    test = ffi.cast("TEST_META_TYPE*", g_tests)[position]
     position = position + 1
-    if position == consts.AB_MAX_NUM_TESTS or test[0].name_hash == 0 or test[0].name_hash == name_hash then
+    if position == consts.AB_MAX_NUM_TESTS or test.name_hash == 0 or test.name_hash == name_hash then
       stop = true
     end
   until stop == true
-  if position < consts.AB_MAX_NUM_TESTS and test[0].name_hash == 0 or test[0].name_hash == name_hash then
-    test[0].name_hash = name_hash
+  if position <= consts.AB_MAX_NUM_TESTS and (test.name_hash == 0 or test.name_hash == name_hash) then
+    test.name_hash = name_hash
     print(position -1)
+    -- c_index[0] = position -1
     return test
   else
-    return nil
+    -- Now search from 0 to orignal position -1
+    position = 0
+    repeat
+      test = ffi.cast("TEST_META_TYPE*", g_tests)[position]
+      position = position + 1
+      if position == original_position or test.name_hash == 0 or test.name_hash == name_hash then
+        stop = true
+      end
+    until stop == true
+    if position <= original_position and (test.name_hash == 0 or test.name_hash == name_hash) then
+      test.name_hash = name_hash
+      print(position -1)
+      -- c_index[0] = position - 1
+      return test
+    else
+      -- c_index[0] = -1
+      assert(nil, "Unable to find any position for insertion")
+    end
   end
 end
 
-function Tests.add(test_str, g_tests)
+
+function Tests.add(test_str, g_tests, c_index)
   -- print(test_str)
   local test_data = json.decode(test_str)
   local test_type = assert(test_data.TestType, "TestType cannot be nil for test")
@@ -71,6 +111,7 @@ function Tests.add(test_str, g_tests)
     c_test = ffi.cast("TEST_META_TYPE*", c_test)[0]
     ffi.copy(c_test.name, test_data.name)
     c_test.test_type = consts.AB_TEST_TYPE_AB
+    c_test.state = assert(states[test_data.State], "Must have a valid state")
     c_test.id = assert(tonumber(test_data.id), "Must have a valid test id")
     -- c_test.name_hash = spooky_hash.spooky_hash64(c_test.name, ffi.C.strlen(c_test.name), g_seed1) -- TODO remove spooky from ffi
     -- c_test.external_id -- TODO onlt for XY Test
