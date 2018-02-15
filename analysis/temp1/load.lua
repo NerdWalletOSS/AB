5f4f62ca778135a9a5f6f175f2af85ce0eaff687
rsubramonian <rsubramonian@nerdwallet.com>
2018-02-14 21:28:30 -0800

    ok

--- analysis/temp1/load.lua
+++ analysis/temp1/load.lua
@@ -12,4 +12,12 @@ local T_eee = Q.load_csv(datadir .. "eee_1609.txt", M, optargs)
 local M = dofile './meta_clk.lua'
 local T_clk = Q.load_csv(datadir .. "clicks_1609.txt", M, optargs)
 
+--[[
+local mint = Q.min(T_eee.time):eval()
+local maxt = Q.max(T_eee.time):eval()
+print(maxt - mint)
+print(mint, maxt)
+--]]
+
+
 print("ALL DONE")
