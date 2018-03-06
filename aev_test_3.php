3f28b20184ead4d8d6d17af3051b2778c8b3aa87
Ramesh Subramonian <subramonian@gmail.com>
2018-03-06 11:14:33 +0000

    page 3 UI created

--- aev_test_3.php
+++ aev_test_3.php
@@ -12,12 +12,12 @@ require_once "db_get_rows.php";
 require_once "db_get_test.php";
 # -- Check if number of TestID are set.
 if (isset($_GET['TestID'])) {$id = $_GET['TestID'];}
+$T = db_get_test($id);
 require_once "processor/display_logic_aev_test.php";
 $config = config_html($TestType);
-$device    = db_get_rows('device');
-$nD = count($device);
-$T = db_get_test('5');
-var_dump($T);
+
+
+//print("<pre>".print_r($T,true)."</pre>");
 ?>
 
   <div class="container theme-showcase" role="main">
@@ -46,10 +46,30 @@ var_dump($T);
 <tr>
 <table class="table table-striped table-condensed" style="space=5px">
 <thead>
-<?php for ( $i = 0; $i < $nD; $i++ ) { 
+<?php 
+$device    = db_get_rows('device');
+$nD = count($device);
+for ( $i = 0; $i < $nD; $i++ ) { 
   echo "<th>".$device[$i]['name']."</th>";
 } ?>
 </thead>
+<tbody>
+<?php for ( $i = 0; $i < $n_var; $i++ ) { 
+  echo "<tr>";
+  echo "<td>".$T['Variants'][$i]['name']."</td>";
+    for ( $j = 0; $j < $nD; $j++ ) {
+      echo "<td><input type='text' maxlength='3' size='3' name='DxV_".$j."_id' value='".$T['DeviceCrossVariant'][$device[$i]['name']][$i]['percentage']."'</td>";
+        }
+  echo "</tr>";
+
+  } ?>
+<tr>
+<td colspan="3"><button class="btn btn-lg btn-success btn-block" type="submit" id="device_x_variant">Next</button></td>
+<td></td>
+<td colspan="3">  <a href="aev_test_4.php?TestID=<?php echo $id; ?>"><button class="btn btn-lg btn-warning btn-block" >Skip</button></a></td>
+
+</tr>
+</tbody>
 </table>
 </td>
 
@@ -63,21 +83,5 @@ var_dump($T);
 
 
 <!-- /container -->
-<script>
-//prop calculator
-$('.prop').keyup(function () {
-  var sum = 0;
-  $('.prop').each(function() {
-  sum += Number($(this).val());
-<?php if ( isset($TestType) && ($TestType == 'ABTest')) { ?>
-  control=100-sum;
-  $('#control').val(control);
-  $('#TotalProp').val(sum+control);
-<?php } elseif ( isset($TestType) && ($TestType == 'XYTest')){ ?>
-  $('#TotalProp').val(sum);
-<?php  } ?>
-});
-});
-</script>
 <!-- FOOTER -->
 <?php require_once "common/footer.php"; ?>
