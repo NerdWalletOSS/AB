2cbb2e770aa52882f1520fc31ce64c05e3cdd301
rsubramonian <rsubramonian@nerdwallet.com>
2018-03-06 11:11:01 -0800

    Added decision tree interpreter Started implementing filters - still working on schema for that Want to make sure that I can avoid giving the webapp delete privileges

---
+++ dt_interpreter/src/compute_dt.c
@@ -0,0 +1,56 @@
+
+int
+eval(
+  double *in, /* [n_features] */
+  int n_features,
+  DECISION_TREE *dt,
+  int n_dt, /* number of nodes in decision tree */
+  int *ptr_npos,
+  int *ptr_nneg
+  )
+{
+  int status = 0;
+
+  int tidx = 0; // start at root 
+  int *ptr_npos = -1;
+  int *ptr_ppos = -1;
+  for ( ; ; ) { /* infinite loop */
+    int fidx = dt[tidx].feature;
+    double val       = in[fidx];
+    double threshold = dt[tidx].threshold;
+
+    if ( ( dt[tidx].left_child < 0 ) && ( dt[tidx].right_child < 0 ) ) {
+      goto DONE;
+    }
+    if ( val <= threshold ) { 
+      is_left = true; 
+    }
+    else {
+      is_left = false;
+    }
+    if ( ( is_left )  && ( dt[tidx].left_child < 0 ) ) {
+      goto DONE;
+    }
+    if ( ( !is_left )  && ( dt[tidx].right_child < 0 ) ) {
+      goto DONE;
+    }
+    if ( is_left ) { 
+      tidx = dt[tidx].left_child;
+    }
+    else {
+      tidx = dt[tidx].right_child;
+    }
+  }
+DONE:
+  int *ptr_npos = dt[tidx].npos;
+  int *ptr_nneg = dt[tidx].nneg;
+BYE:
+  return status;
+}
+
+
+
+
+  
+
+
