44dd034ea7e488800a6923cca8e6d6f3c17abdba
Ramesh Subramonian <subramonian@gmail.com>
2018-03-15 19:50:02 +0000

    page 3 complte plus page 4 UI Logic build inside the html

--- aev_test_4.php
+++ aev_test_4.php
@@ -14,12 +14,12 @@ require_once "find_tests_to_follow.php";
 # -- Check if number of TestID are set.
 if (isset($_GET['TestID'])) {$id = $_GET['TestID'];}
 $T = db_get_test($id);
+
 require_once "processor/display_logic_aev_test.php";
 $config = config_html($TestType);
 $fo_tests =  find_tests_to_follow($Channel);
-var_dump($GLOBALS['state']); exit;
+//var_dump($GLOBALS['state']); exit;
 $nF = count($fo_tests);
-
 //print("<pre>".print_r($T,true)."</pre>");
 ?>
 
@@ -48,27 +48,48 @@ $nF = count($fo_tests);
   </td>
 	</tr>
 <tr>
-<table class="table table-striped table-condensed" style="space=5px">
-<thead>
-<th>TestID</th>
-<th>TestName</th>
-<th>&nbsp;</th>
-</thead>
-<tbody>
+<td>
 <?php 
-for ( $fidx = 0; $i < $nF; $fidx++ ) { 
+if (($T['State'] == "draft") && ( $nF == 0 ) ){
+  echo "<strong>No Test to Follow</strong>";
+} elseif (($T['State'] == "draft") && ( $nF != 0 ) ) {
+    echo "<form method='post'>";
+for ( $fidx = 0; $fidx < $nF; $fidx++ ) { 
+  echo "<table class='table table-striped table-condensed' style='space=5px>";
+  echo "<thead>";
+  echo "<th>TestID</th>";
+  echo "<th>TestName</th>";
+  echo "<th>Select</th>";
+  echo "</thead>";
+  echo "<tbody>";
   echo "<tr>";
   echo "<td>".$fo_tests[$fidx]['id']."<input type='hidden' name='TID_to_follow' value='".$fo_tests[$fidx]['id']."'></td>";
   echo "<td>".$fo_tests[$fidx]['name']."<input type='hidden' name='TNAME_to_follow' value='".$fo_tests[$fidx]['name']."'></td>";
+  echo "<td><input type='radio' name='follow' value='".$fo_tests[$fidx]['id']."'></td>";
   echo "</tr>";
-  } ?>
+  echo "<tr>";
+  echo "<td colspan='3'><button class='btn btn-lg btn-success btn-block' type='submit' id='follow_on'>Next</button></td>";
+  //8934988782
+  echo "</tr>";
+  } 
+  echo "<tr>";
+  echo "</form>";
+  echo "<td></td>";
+  echo "<td colspan='3'>  <a href='home.php'><button class='btn btn-lg btn-warning btn-block' >Skip</button></a></td>";
+  echo "</tr>";
+} elseif(($T['State'] != "draft") && ($T['pred_id'] == "")) {
+    echo "<strong>Not following anybody</strong>";
+} elseif(($T['State'] != "draft") && ($T['pred_id'] != "") ){
+    $following = db_get_test($T['pred_id']);
+    echo "Following ".$following['name'];
+}
+  else {
+  // Do Nothing;
+}
+?>
+</td>
 <tr>
-<td colspan="3"><button class="btn btn-lg btn-success btn-block" type="submit" id="follow_on">Next</button></td>
-</form>
-<td></td>
-<td colspan="3">  <a href="thankyou.php?TestID=<?php echo $id; ?>"><button class="btn btn-lg btn-warning btn-block" >Skip</button></a></td>
-
-8934988782
+<td colspan='3'>  <a href='home.php'><button class='btn btn-lg btn-warning btn-block' >Skip</button></a></td
 </tr>
 </tbody>
 </table>
--- processor/page_3_processor.php
+++ processor/page_3_processor.php
@@ -16,24 +16,26 @@ if ( !$_POST ) {
   echo '{ "InsertTest" : "ERROR", "Message" : "No paylaod" }'; exit;
 }
 $test_id = $_POST['TestID'];
-$TestType = $_POST['TestType'];
 // SETTING NEW VALUES OF DEVICE X VARIANT
 if (isset($_POST['is_dev_specific'])) {
-$T['is_dev_specific'] = $_POST['is_dev_specific'];
+  $T['is_dev_specific'] = $_POST['is_dev_specific'];
 }
 $T = db_get_test($test_id);
-$n_var = count($T['Variants']);
-$device    = db_get_rows('device');
-$nD = count($device);
-for ( $i = 0; $i < $n_var; $i++ ) { 
+$nV = count($T['Variants']);
+$this_device    = db_get_rows('device');
+$nD = count($this_device);
+for ( $i = 0; $i < $nV; $i++ ) { 
   for ( $j = 0; $j < $nD; $j++ ) {
-    $T['DeviceCrossVariant'][$device[$j]['name']][$i]['percentage'] = $_POST[$device[$j]["name"].'_'.$i];
+    $T['DeviceCrossVariant'][$this_device[$j]['name']][$i]['percentage'] = $_POST[$this_device[$j]["name"].'_'.$i];
   }
 }
+
 //-------------------------------------
 // Call to Add/Edit Additional Variant Information
 //print("<pre>".print_r($T,true)."</pre>");
-$rslt =  set_device_specific_variant(json_encode($T)); //add_addnl_var_info(json_encode($X));
-header("TestID: ".$id);
+//var_dump(json_encode($T));
+//exit();
+$rslt =  set_device_specific_variant(json_encode($T));
+header("TestID: ".$rslt["TestID"]);
 ob_clean();
 ?>
