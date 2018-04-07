6b940d7f8010532b1e8ada79e3a776940dec57f3
rsubramonian <rsubramonian@nerdwallet.com>
2018-04-06 21:52:43 -0700

    ok

---
+++ test_webapp/bad_basic3.lua
@@ -0,0 +1,24 @@
+return 
+{
+  TestType =  "XYTest",
+  Channel =  "facebook",
+  BinType =  "anonymous",
+  description =  "some bogus description",
+  Creator =  "joe",
+  name =  "Test1",
+  Variants =  { {
+    name =  "Control",
+    percentage =  "-1",
+    url =  "www.yahoo.com"
+  },
+  {
+    name =  "Variant_A",
+    percentage =  "101",
+    url =  "www.yahoo.com"
+  },
+  {
+    name =  "Variant_B",
+    percentage =  "0",
+    url =  "www.cnn.com"
+  }, }
+}
