f3fbb23cad61406f7a19a8d13ad9074638004924
rsubramonian <rsubramonian@nerdwallet.com>
2018-04-06 18:50:47 -0700

    ok

--- test_webapp/test1.lua
+++ test_webapp/test1.lua
@@ -3,6 +3,9 @@ local JSON = (loadfile "JSON.lua")() -- one-time load of the routines
 local cURL = require "cURL"
 local ltn12 = require("ltn12") -- TODO What is this for?
 local plstr = require 'pl.stringx'
+local plpath = require 'pl.path'
+local get_test_id = require 'get_test_id'
+local get_error_code = require 'get_error_code'
 local function trim1(s)
   return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
@@ -19,6 +22,13 @@ end
 --============================
 -- START: Stuff common to all tests in this suite
   url = "localhost:8080/AB/php/endpoints/endpoint_test_basic.php"
+  curl_params = { 
+    url        = url,
+    post       = true,
+    httpheader = { "Content-Type: application/json"; },
+    writefunction  = snarf_body,
+    headerfunction = snarf_headers,
+  }
 -- STOP: Stuff common to all tests in this suite
 local tests = {}
 tests.t1 = function (
@@ -45,26 +55,17 @@ tests.t1 = function (
   Variants[#Variants+1] = v
   --==============
   T.Variants = Variants
-  local c = cURL.easy{
-    url        = url,
-    post       = true,
-    postfields = JSON:encode(T),
-    httpheader = { "Content-Type: application/json"; };
-    writefunction  = snarf_body,
-    headerfunction = snarf_headers,
-  }
-  
+
+  curl_params.postfields = JSON:encode(T)
+  local c = cURL.easy(curl_params)
   x = c:perform()
-  local test_id 
-  for k, v in ipairs(hdrs) do 
-    if ( string.find(v, "TestID") == 1 ) then 
-      test_id = assert(string.gsub(v, "TestID: ", ""))
-      test_id = assert(tonumber(test_id))
-    end
-  end
+
+  local test_id = assert(get_test_id(hdrs))
   assert(test_id > 0)
+  local error_code = assert(get_error_code(hdrs))
+  assert(error_code == 200)
   print("Test t1 succeeded")
-  return T, c
+  return T
 end
 tests.t2 = function (
   just_pr
@@ -73,22 +74,72 @@ tests.t2 = function (
   if ( just_pr ) then print(description) return end
 
   os.execute("cd ../sql/; bash reset_db.sh; cd - ")
-  local T, c = tests.t1()
+  local T = tests.t1()
   hdrs = {} -- IMPORTANT: Needs to be reset because set in t1()
   body = "" -- IMPORTANT: Needs to be reset because set in t1()
-  local x = c:perform()
-  local error_code
-  for k, v in ipairs(hdrs) do 
-    local needle = 'Error%-Code:' -- REMEMBER TO ESCAPE THE HYPHEN
-    if ( string.find(v, needle) == 1 ) then 
-      error_code = assert(string.gsub(v, needle, ""))
-      error_code = assert(tonumber(error_code))
-    end
-  end
+
+  curl_params.postfields = JSON:encode(T)
+  local c = cURL.easy(curl_params)
+  x = c:perform()
+
+  local error_code = get_error_code(hdrs)
   assert(error_code == 400)
   print("Test t2 succeeded")
   return true
 end
+--===================================================
+tests.t3 = function (
+  just_pr
+  )
+  description = "Create a number of valid tests "
+  if ( just_pr ) then print(description) return end
+
+  for i = 1, 100000 do 
+    os.execute("cd ../sql/; bash reset_db.sh; cd - ")
+    local filename = "basic" .. i .. ".lua"
+    if ( not plpath.isfile(filename) )  then break end 
+    T = dofile("good_basic" .. i .. ".lua")
+    hdrs = {} 
+    body = "" 
+    curl_params.postfields = JSON:encode(T)
+    local c = cURL.easy(curl_params)
+    x = c:perform()
+    local test_id = assert(get_test_id(hdrs))
+    assert(test_id > 0)
+    local error_code = assert(get_error_code(hdrs))
+    assert(error_code == 200)
+    print("Tested " .. filename)
+  end
+  print("Test t3 succeeded")
+  return true
+end
+--===================================================
+tests.t4 = function (
+  just_pr
+  )
+  description = "Create a number of invalid tests "
+  if ( just_pr ) then print(description) return end
+
+  for i = 1, 100000 do 
+    os.execute("cd ../sql/; bash reset_db.sh; cd - ")
+    local filename = "basic" .. i .. ".lua"
+    if ( not plpath.isfile(filename) )  then break end 
+    T = dofile("bad_basic" .. i .. ".lua")
+    hdrs = {} 
+    body = "" 
+    curl_params.postfields = JSON:encode(T)
+    local c = cURL.easy(curl_params)
+    x = c:perform()
+    local error_code = assert(get_error_code(hdrs))
+    assert(error_code == 400)
+    print("Tested " .. filename)
+  end
+  print("Test t4 succeeded")
+  return true
+end
+--===================================================
 tests.t1() -- TODO: DELETE ONCE I FIGURE OUT WHY TESTRUNNER NOT WORKING HERE
 tests.t2() -- TODO: DELETE ONCE I FIGURE OUT WHY TESTRUNNER NOT WORKING HERE
+tests.t3() -- TODO: DELETE ONCE I FIGURE OUT WHY TESTRUNNER NOT WORKING HERE
+tests.t4() -- TODO: DELETE ONCE I FIGURE OUT WHY TESTRUNNER NOT WORKING HERE
 return tests
