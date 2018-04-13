bcb9dd2dd71fc6f057f9014ce5679cf52999ebb2
rsubramonian <rsubramonian@nerdwallet.com>
2018-04-13 14:52:43 -0700

    ok

---
+++ src/get_date.c
@@ -0,0 +1,33 @@
+#include "ab_incs.h"
+#include "auxil.h"
+#include "get_date.h"
+
+int 
+get_date(
+    struct evhttp_request *req,
+    char *date,
+    size_t sz_date
+    )
+{
+  int status = 0;
+
+  if ( date == NULL ) { go_BYE(-1); }
+  if ( sz_date <= 1 ) { go_BYE(-1); }
+  memset(date, '\0', sz_date+1);
+  struct evkeyvalq *headers = NULL;
+  struct evkeyval  *header = NULL;
+  headers = evhttp_request_get_input_headers (req);
+  header = headers->tqh_first;
+  for (header = headers->tqh_first; header; 
+      header = header->next.tqe_next) {
+    fprintf(stderr, "%s\n", header->key);
+    if ( strcasecmp(header->key, "Date") == 0 ) { 
+      if ( strlen(header->value ) <= sz_date ) { 
+        strcpy(date, header->value);
+      }
+      break; 
+    }
+  }
+BYE:
+  return status;
+}
