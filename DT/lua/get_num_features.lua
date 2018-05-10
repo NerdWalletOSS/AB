4e9ba4beaa2eabda6d1f9e5cb45ae7a99b753924
jianyanglum <jian.yang.lum@gmail.com>
2018-05-10 11:08:12 -0700

    some git issues; now we have better tracking

---
+++ DT/lua/get_num_features.lua
@@ -0,0 +1,26 @@
+package.path = package.path .. ';../../?.lua'
+local cache = require 'lua_common.cache'
+
+local function get_nested_length(table)
+  --[[
+  Get the number of nested elements in a table. I.e. if a table contains a k, v pair
+  with v as a table, counts the number of nested elements in that, and adds to the current count.
+  ]]--
+  local counter = 0
+  for k, v in pairs(table) do
+    if type(v) == 'table' then
+      counter = counter + get_nested_length(v)
+    else
+      counter = counter + 1
+    end
+  end
+  return counter
+end
+
+local function get_num_features()
+  local dt_feature = assert(cache.get("dt_feature"),
+    'dt_feature not in cache.')
+  return get_nested_length(dt_feature)
+end
+
+return get_num_features
---
+++ DT/lua/post_proc_preds.lua
@@ -0,0 +1,44 @@
+package.path = package.path .. ';../../?.lua'
+local ffi = require 'ffi'
+local assertx = require 'lua_common.assertx'
+local cache = require 'lua_common.cache'
+local json = require 'lua.json'
+
+
+local function get_mdl_ct(mdl_map)
+  assert(mdl_map[0], 'mdl_map must be 0-indexed')
+  local count = 0
+  for k, v in pairs(mdl_map) do count = count + 1 end
+  return count
+end
+
+
+local function post_proc_preds(
+  opvec, -- feature vector, now a float* of n_opvec length
+  n_opvec, -- number of elements in feature vector. should be 21
+  out_buf, -- output buffer for debugging 
+  sz_out_buf -- size of above
+  )
+  opvec = ffi.cast("float*", opvec)
+  local out_features = {}
+  local mdl_map = assert(cache.get('mdl_map'),
+    'mdl_map missing from cache.')
+  local n_mdl = assert(get_mdl_ct(mdl_map), 'mdl_map failed.')
+  assertx(n_opvec == n_mdl, 'feature vector length is ',
+    tostring(n_opvec), ', but mdl_map says length is ', 
+    tostring(n_mdl))
+  for idx, mdl in pairs(mdl_map) do
+    mdl_n = assert(tonumber(mdl), 'mdl_map faulty')
+    local pred = assertx(opvec[tonumber(idx)], 'Index ', tostring(idx),
+      ' not present in opvec.')
+    assertx(0 <= pred and pred <= 1, 'prediction ', tostring(pred), 
+      ' not between 0 and 1 inclusive.')
+    out_features[mdl_n] = pred
+  end
+  local x = assert(json.encode(out_features))
+  assertx(#x <= sz_out_buf, 'len of string is ', #x,
+    ', which is too long for the buffer with length ', sz_out_buf)
+  ffi.copy(out_buf, x)
+end
+
+return post_proc_preds
