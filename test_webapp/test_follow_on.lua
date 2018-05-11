60e624ee06ba6fa1da566eedc22a5e480f01e50f
rsubramonian <rsubramonian@nerdwallet.com>
2018-05-11 10:39:20 -0700

    ok

--- test_webapp/test_follow_on.lua
+++ test_webapp/test_follow_on.lua
@@ -14,6 +14,23 @@ local set_follow_on = require 'set_follow_on'
 local S = require 'state_change'
 
 local states = require 'states'
+local function mk_and_archive(optargs)
+  assert(type(optargs) == "table")
+  local T1 = dofile("good_basic1.lua")
+  if ( optargs.name )     then T1.name     = optargs.name end 
+  if ( optargs.TestType ) then T1.TestType = optargs.TestType end 
+  if ( optargs.Channel )  then T1.Channel  = optargs.Channel end 
+
+  local hdrs, outbody, status = mk_test(T1)
+  -- print(outbody); for k, v in pairs(hdrs) do print(k,v) end 
+  assert(status == 200)
+  local t1_id = get_test_id(hdrs)
+  S.publish(t1_id)
+  S.start(t1_id)
+  S.terminate(t1_id)
+  S.archive(t1_id)
+  return t1_id
+end
 --==========================
 
 local tests = {}
@@ -28,22 +45,14 @@ tests.t1 = function (
   if ( just_pr ) then print(description) return end
 
   reset_db()
-  -- Make test T1 and archive it 
-  local T1 = dofile("good_basic1.lua")
-  T1.TestType = "XYTest"
-  T1.Channel = "facebook"
-  local hdrs, outbody, status = mk_test(T1)
-  assert(status == 200)
-  local t1_id = get_test_id(hdrs)
-  local T2 = get_test_info(t1_id)
-  S.publish(t1_id)
-  S.start(t1_id)
-  S.terminate(t1_id)
-  S.archive(t1_id)
+  local X = {}; X.TestType = "XYTest"; X.Channel = "facebook"
+  local t1_id = assert(mk_and_archive(X))
 
   -- Make test T2 with same channel as T
+  local T2 = get_test_info(t1_id)
   T2.name = "Test2"
   T2.id = nil
+  T2.State = nil
   local hdrs, outbody, status = mk_test(T2)
   assert(status == 200)
 
@@ -65,22 +74,14 @@ tests.t2 = function (
   if ( just_pr ) then print(description) return end
 
   reset_db()
-  -- Make test T1 and archive it 
-  local T1 = dofile("good_basic1.lua")
-  T1.TestType = "XYTest"
-  T1.Channel = "facebook"
-  local hdrs, outbody, status = mk_test(T1)
-  assert(status == 200)
-  local t1_id = get_test_id(hdrs)
-  local T2 = get_test_info(t1_id)
-  S.publish(t1_id)
-  S.start(t1_id)
-  S.terminate(t1_id)
-  S.archive(t1_id)
+  local X = {}; X.TestType = "XYTest"; X.Channel = "facebook"
+  local t1_id = assert(mk_and_archive(X))
 
   -- Make test T2 with same channel as T
+  local T2 = get_test_info(t1_id)
   T2.name = "Test2"
   T2.id = nil
+  T2.State = nil
   local hdrs, outbody, status = mk_test(T2)
   assert(status == 200)
   local t2_id = get_test_id(hdrs)
