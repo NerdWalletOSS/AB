58f48d4e850ead69e3ceff213ca1e10d48a583b2
rsubramonian <rsubramonian@nerdwallet.com>
2018-06-01 12:53:50 -0700

    ok

---
+++ test_rts/test2.lua
@@ -0,0 +1,36 @@
+local curl = require 'lua/curl'
+local JSON = require 'lua/JSON'
+local reload_db = require 'test_webapp/reload_db'
+local plpath = require 'pl.path'
+
+local ltc_url = "localhost:8000/ListTests?Source=C&TestType=XYTest"
+local ltl_url = "localhost:8000/ListTests?Source=C&TestType=XYTest"
+local dc_url = "localhost:8000/Diagnostics?Source=C"
+local dl_url = "localhost:8000/Diagnostics?Source=Lua"
+
+local tests = {}
+tests.t1 = function(
+  num_iters
+  )
+  --=======================================
+  if ( not num_iters) then num_iters = 10 end 
+  x = plpath.currentdir()
+  print(x)
+  reload_db(plpath.currentdir() .. "/abdb2_100.sql") -- TODO Improve path
+
+  for i = 1, num_iters do 
+    a, b, c = curl.get("localhost:8000/Reload"); assert(c == 200)
+    a, b, c = curl.get(ltc_url);   assert(c == 200)
+    local Lc = JSON:decode(b)
+
+    a, b, c = curl.get(ltl_url);   assert(c == 200)
+    local Ll = JSON:decode(b)
+    assert(#Ll == #Lc)
+    assert(#Ll == 100)
+
+    a, b, c = curl.get(dl_url);   assert(c == 200)
+    a, b, c = curl.get(dc_url);   assert(c == 200)
+  end
+end
+-- tests.t1()
+return tests
