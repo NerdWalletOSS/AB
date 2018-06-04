876d405916f16d1c285a36f1082b1ea0313067fe
rsubramonian <rsubramonian@nerdwallet.com>
2018-06-04 16:21:51 -0700

    lots more testing of RTS

---
+++ test_rts/rand_set.lua
@@ -0,0 +1,64 @@
+local states = { "started", "terminated", "archived" }
+local test_types = { "ABTest", "XYTest" }
+local is_dev_specific = { 
+  "0", 
+  -- "1", 
+}
+local channels = {
+'aol',
+'bing',
+'cnn',
+'dianomi',
+'facebook',
+'google',
+'instagram',
+'outbrain',
+'pandora',
+'pinterest',
+'pocket',
+'taboola',
+'twitter',
+'weather_channel',
+'yahoo',
+'youtube',
+}
+local rts_endpoints = {
+"add_test",
+-- "chk_db_conn",
+-- "chk_test",
+-- "diagnostics",
+-- "get_config",
+-- "list_tests",
+-- "reload",
+-- "reset_db",
+-- "test_info",
+-- "update_config",
+}
+
+local function is_in_set(
+  sname, 
+  sets
+  )
+  for _, v in pairs(sets) do
+    if ( v == sname ) then return true end
+  end
+  return false
+end
+
+
+local sets = { }
+sets["states"] = states
+sets["test_types"]   = test_types
+sets["is_dev_specific"] = is_dev_specific
+sets["channels"] = channels
+sets["rts_endpoints"] = rts_endpoints
+
+local function rand_set(sname)
+  assert(type(sname) == "string")
+  local S = assert(sets[sname], "no set with name " .. sname)
+  assert(type(S) == "table")
+  local sidx = math.random(#S) 
+  assert(S[sidx], "no value for index = " .. sidx .. " and set = " .. sname)
+  return S[sidx]
+end
+return rand_set
---
+++ test_rts/test_daft.lua
@@ -0,0 +1,38 @@
+local curl              = require 'lua/curl'
+local JSON              = require 'lua/JSON'
+local compare_test_info = require 'test_rts/compare_test_info'
+local test_daft         = require 'test_webapp/test_daft'
+local rand_set          = require 'test_rts/rand_set'
+local rand_test         = require 'test_rts/rand_test'
+
+local at_url = "localhost:8000/AddTest"
+
+local tic_url = "localhost:8000/TestInfo?Source=C&TestType=XYTest"
+local til_url = "localhost:8000/TestInfo?Source=Lua&TestType=XYTest"
+
+local ltc_url = "localhost:8000/ListTests?Source=C&TestType=XYTest"
+local ltl_url = "localhost:8000/ListTests?Source=C&TestType=XYTest"
+
+local dc_url = "localhost:8000/Diagnostics?Source=C"
+local dl_url = "localhost:8000/Diagnostics?Source=Lua"
+
+local niters = 100
+local tests = {}
+tests.t1 = function(num_iters, num_hits)
+  if ( not num_iters ) then num_iters = 100 end 
+  if ( not num_hits ) then num_hits   = 100 end 
+  local a, b, c = curl.get("localhost:8000/Restart"); assert(c == 200)
+  for i = 1, num_iters do 
+    local choice = rand_set("rts_endpoints")
+    if ( choice == "add_test" ) then 
+      local T = rand_test()
+      print(i, T.TestType, T.State, T.final_variant_idx, T.final_variant_id)
+      a, b, c = curl.get(at_url, nil, JSON:encode(T))
+    else
+      assert(nil, "not implemented" .. choice)
+    end
+
+  end
+end
+tests.t1(100)
+return tests
