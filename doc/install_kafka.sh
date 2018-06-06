695342a7b7c8093da581a4135dc3b63eff8725f9
Indrajeet Singh <isingh@nerdwallet.com>
2018-06-06 10:29:25 -0400

    adding fake consumer

--- doc/install_kafka.sh
+++ doc/install_kafka.sh
@@ -1,4 +1,4 @@
-#!/bin/ bash
+#!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo add-apt-repository -y ppa:webupd8team/java
@@ -7,8 +7,9 @@ sudo apt-get install oracle-java8-installer -y
 sudo apt-get install zookeeperd
 netstat -ant | grep :2181
 
-wget http://mirror.fibergrid.in/apache/kafka/0.10.0.1/kafka_2.10-0.10.0.1.tgz
-wget "http://www-eu.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz"
+# wget http://mirror.fibergrid.in/apache/kafka/0.10.0.1/kafka_2.10-0.10.0.1.tgz
+# wget "http://www-eu.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz"
+wget http://www.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz
 sudo rm -rf /opt/kafka/
 sudo mkdir -p /opt/kafka
 sudo tar -xvzf kafka_2.12-1.0.1.tgz --directory /opt/kafka --strip-components 1
