6ce8ddfdee8717f03f25a690428a7f5d139b3eb6
i <i@ljabn.east1.stage.nerdwallet.biz>
2018-06-12 14:58:56 -0400

    removing extra user agent folder

--- user_agent_classifier/src/load_names.h
+++
@@ -1,6 +0,0 @@
-extern int
-load_names(
-    const char *names_file,
-    MODEL_NAME_TYPE **ptr_M,
-    int *ptr_nM
-    );
--- user_agent_classifier/src/score_url.h
+++
@@ -1,9 +0,0 @@
-extern int
-score_url(
-    char *url, 
-    MODEL_REC_TYPE *M,
-    size_t nM,
-    MODEL_NAME_TYPE *N,
-    size_t num_models,
-    int *ptr_best_model
-    );
