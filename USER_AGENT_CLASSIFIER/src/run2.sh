a4dc2b099cf3328b8c80ebac2dd2c003db9bba9d
rsubramonian <rsubramonian@nerdwallet.com>
2018-06-13 15:06:36 -0700

    ok

--- USER_AGENT_CLASSIFIER/src/run2.sh
+++ USER_AGENT_CLASSIFIER/src/run2.sh
@@ -9,5 +9,6 @@ uafile=../data/rs_tmp_user_agent000
 opfile=_ua_to_dev_map.bin
 test -f $uafile
 $VG ./mk_ua_to_dev_map $uafile $opfile  1>_out 2>&1
+test -f $opfile
 grep "0 errors" _out  1>/dev/null 2>&1
 echo "Successfully completed $0 in $PWD"
