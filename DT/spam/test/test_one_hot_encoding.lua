56f1763e5e469ca05c9f424ee6fa629c83339b6d
jianyanglum <jian.yang.lum@gmail.com>
2018-06-14 14:48:32 -0700

    Added changes to test files to conform to style;

--- DT/spam/test/test_one_hot_encoding.lua
+++ DT/spam/test/test_one_hot_encoding.lua
@@ -1,4 +1,4 @@
-require 'busted.runner'()
+require "busted.runner"()
 local plfile           = require "pl.file"
 local plpath           = require "pl.path"
 local assertx          = require "lua/assertx"
@@ -12,18 +12,18 @@ cache.put("dt_feature", dt_feature)
 describe("Testing one_hot_encoding", function()
 
   it("should be able to return the correct one_hot_encoding for test data.", function()
-  local test_data = "ohe_data.json"
-  assert(plpath.isfile(test_data))
-  local json_str = assert(plfile.read(test_data))
-  local json_dict = assert(JSON:decode(json_str))
-  assert(type(json_dict) == "table")
+    local test_data = "ohe_data.json"
+    assert(plpath.isfile(test_data))
+    local json_str = assert(plfile.read(test_data))
+    local json_dict = assert(JSON:decode(json_str))
+    assert(type(json_dict) == "table")
 
-  for _,pair in ipairs(json_dict) do
-    local lua_ohe = one_hot_encoding(pair)
-    for k,v in pairs(pair) do
-      lua_ohe[tonumber(k)] = tonumber(v)
-    end
-    assert.are.same(lua_ohe, pair)
+    for _,pair in ipairs(json_dict) do
+      local lua_ohe = one_hot_encoding(pair)
+      for k,v in pairs(pair) do
+        lua_ohe[tonumber(k)] = tonumber(v)
+      end
+      assert.are.same(lua_ohe, pair)
     end
   end)
 end)
