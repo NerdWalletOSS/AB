c65793b1cb9eb1f28726ef7a08dc41901a9f1f9a
Indrajeet Singh <isingh@nerdwallet.com>
2018-06-19 09:52:14 -0400

    Removing kafka commands

--- aio.sh
+++ aio.sh
@@ -10,7 +10,6 @@ usage(){
   echo "  -t  Installs testing utils"
   echo "  -r  Builds AB and runs tests"
   echo "  -x  Builds a tarball of all the php companents for AB admin"
-  echo "  -s  Builds and starts all support systems"
 
   # echo "  -r  Builds and run AB with all other auxillary service"
   exit 1 ;
@@ -69,59 +68,6 @@ build(){
   cd -
 }
 
-clean_kafka_logs(){
-  sudo rm -f /opt/kafka/logs/*
-}
-
-install_kafka(){
-  sudo apt-get update
-  sudo apt-get install default-jre -y
-  sudo apt-get install zookeeperd zookeeper -y
-  RES="`echo ruok | nc localhost 2181`"
-  if [[ "$RES" != "imok" ]]
-  then
-    echo "Zookeeper is not working\n"
-    exit 1
-  fi
-  sudo adduser --system --no-create-home --disabled-password --disabled-login kafka
-  wget "http://www-eu.apache.org/dist/kafka/1.0.1/kafka_2.12-1.0.1.tgz"
-  curl http://kafka.apache.org/KEYS | gpg --import
-  wget https://dist.apache.org/repos/dist/release/kafka/1.0.1/kafka_2.12-1.0.1.tgz.asc
-  gpg --verify kafka_2.12-1.0.1.tgz.asc kafka_2.12-1.0.1.tgz
-  sudo mkdir /opt/kafka
-  sudo tar -xvzf kafka_2.12-1.0.1.tgz --directory /opt/kafka --strip-components 1
-  rm -rf kafka_2.12-1.0.1.tgz kafka_2.12-1.0.1.tgz.asc
-  sudo mkdir /var/lib/kafka
-  sudo mkdir /var/lib/kafka/data
-  sudo chown -R kafka:nogroup /opt/kafka
-  sudo chown -R kafka:nogroup /var/lib/kafka
-  sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
-  /opt/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ab &
-  kill -9 %2
-  /opt/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181 | grep "ab"
-  RES="`echo $?`"
-  if [[ "$RES" != "0" ]]
-  then
-    echo "Unable to get registered topic ab"
-    exit 1
-  fi
-  kill -9 %1
-}
-
-run_kafka(){
-  sudo ls /opt/kafka/bin/kafka-server-start.sh
-  RES="`echo $?`"
-  if [ $RES -ne 0 ]
-  then
-    install_kafka
-  fi
-  if [ "`ps auxwwf | grep "kafka-server-start" | grep -v "grep" | wc -l`" -ne 1 ]
-  then
-    sudo /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &>/dev/null &
-    /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic test --from-beginning &
-  fi
-
-}
 
 run_logger(){
   if [ "`ps auxwwf | grep "ab_logger" | grep -v "grep" | wc -l`" -ne 1 ]
@@ -136,12 +82,6 @@ run_logger(){
   fi
 }
 
-
-start_support_systems(){
-  clean_kafka_logs
-  run_kafka
-  run_logger
-}
 
 
 install_mysql(){
@@ -208,13 +148,8 @@ install_deps_from_source() {
   sudo luarocks install busted Lua-cURL luasec
 }
 
-install_travis(){
-  build
 
-}
-
-
-while getopts "sbochptx" opt;
+while getopts "bochptx" opt;
 do
   case $opt in
     h)
@@ -225,10 +160,6 @@ do
       build
       tar -cvzf bin.tar.gz ./bin
       tar -cvzf php.tar.gz php app
-      exit 0
-      ;;
-    s)
-      start_support_systems
       exit 0
       ;;
     b)
