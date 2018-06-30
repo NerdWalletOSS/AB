e39bf6b725fd258434f81e5ef37feb9ddbf58a6e
rsubramonian <rsubramonian@nerdwallet.com>
2018-06-29 17:06:23 -0700

    fixing config file bugs

--- RTS/ab_load_config.lua
+++ RTS/ab_load_config.lua
@@ -150,6 +150,7 @@ end
 local function update_rts_configs(g_cfg, config)
   local c_struct = g_cfg[0]
   local is_updated = consts.FALSE
+  print("XXXX",   config.PORT.VALUE, c_struct.port)
   is_updated,  c_struct.port = update_number_field(config.PORT, c_struct.port, is_updated, 0,
   2^16-1)
   if is_present(config.VERBOSE) then
--- src/statsd.h
+++ src/statsd.h
@@ -1,46 +1,5 @@
-#include "ab_globals.h"
-
-inline int STATSD_INC(char *stat){
-  int status = 0;
-  if (g_statsd_link != NULL ) {
-    status = statsd_inc(g_statsd_link, stat, 1.0);
-  }
-  return status;
-}
-
-inline int STATSD_DEC(char *stat){
-  int status = 0;
-  if (g_statsd_link != NULL ) {
-    snprintf(g_statsd_buf, AB_MAX_LEN_REDIRECT_URL, "%s.%s", g_cfg.statsd_inc, stat);
-    status = statsd_dec(g_statsd_link, g_statsd_buf, 1.0);
-  }
-  return status;
-}
-
-
-inline int STATSD_COUNT(char *stat, size_t cnt){
-  int status = 0;
-  if (g_statsd_link != NULL ) {
-    snprintf(g_statsd_buf, AB_MAX_LEN_REDIRECT_URL, "%s.%s", g_cfg.statsd_count, stat);
-    status = statsd_count(g_statsd_link, g_statsd_buf, cnt, 1.0);
-  }
-  return status;
-}
-
-inline int STATSD_GAUGE(char *stat, size_t value){
-  int status = 0;
-  if (g_statsd_link != NULL ) {
-    snprintf(g_statsd_buf, AB_MAX_LEN_REDIRECT_URL, "%s.%s", g_cfg.statsd_gauge, stat);
-    status = statsd_gauge(g_statsd_link, g_statsd_buf, value);
-  }
-  return status;
-}
-
-inline int STATSD_TIMING(char *stat, size_t ms){
-  int status = 0;
-  if (g_statsd_link != NULL ) {
-   snprintf(g_statsd_buf, AB_MAX_LEN_REDIRECT_URL, "%s.%s", g_cfg.statsd_inc, stat);
-   status = statsd_timing(g_statsd_link, g_statsd_buf, ms);
-  }
-  return status;
-}
+extern int STATSD_INC(const char *stat);
+extern int STATSD_DEC(const char *stat);
+extern int STATSD_COUNT(const char *stat, size_t cnt);
+extern int STATSD_GAUGE(const char *stat, size_t value);
+extern int STATSD_TIMING(const char *stat, size_t ms);
