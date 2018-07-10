7f7767f7068382ba7a9a3d08817fc726a6eab8f4
rsubramonian <rsubramonian@nerdwallet.com>
2018-07-10 13:14:16 -0700

    more testing

--- test_webapp/mk_rand_test.lua
+++ test_webapp/mk_rand_test.lua
@@ -2,16 +2,13 @@ require 'lua/str'
 local JSON    = require 'lua/JSON'
 local curl    = require 'lua/curl'
 local tbp_url = "localhost:8080/AB/php/endpoints/endpoint_test_basic.php"
-
 local R = require 'test_webapp/rand_selections'
 
 local function mk_rand_test(
   X
   )
   -- TODO Following is a hack. Needs to be done properly
-  local T = {}
-  if ( not X ) then X = {} end 
-  T.name = X.name or tostring(math.random(1, 1000000000))
+  local T = R.rand_test(X)
   local hdrs, outbody, status = curl.post(tbp_url, nil, JSON:encode(T))
   assert(status == 200 )
   return get_test_id(hdrs)
--- test_webapp/rand_selections.lua
+++ test_webapp/rand_selections.lua
@@ -2,12 +2,51 @@ local channels = require 'test_webapp/channels'
 local admins   = require 'test_webapp/admins'
 -- TODO P3 Random selections should be improved
 local R = {}
+
+local function rand_perc(
+  TestType,
+  NumVariants
+  )
+  math.randomseed(os.time())
+  local P = {}
+  if ( TestType == "XYTest" ) then
+    local sum_perc = 0
+    local balance = 100
+    local perc
+    for i = 1, NumVariants-1 do 
+      if ( balance == 0 ) then 
+        perc = 0
+      else
+        perc = math.random(0, balance)
+        balance = balance - perc
+      end
+      P[i] = perc
+    end
+    -- last guy gets whateveris left over
+    P[NumVariants] = balance
+  elseif ( TestType == "ABTest" ) then
+    print("TODO: no change to perc as yet")
+  else
+    assert(nil)
+  end
+  return P
+end
+
 local function rand_name()
   return tostring(math.random(1, 1000000000))
 end
+
 local function rand_dscr()
   return tostring(math.random(1, 1000000000))
 end
+
+local function rand_type()
+  local x = math.random(1, 2)
+  if ( x == 1 ) then return "ABTest" end 
+  if ( x == 2 ) then return "XYTest" end 
+  assert(nil)
+end
+
 local function rand_chnl()
   local nC = #channels
   local cidx = math.random(0, nC)
@@ -17,7 +56,7 @@ local function rand_chnl()
     return channels[cidx]
   end
 end
-local function rand_dev_specific()
+local function rand_dvsp()
   return math.random(0, 1)
 end
 local function rand_admn()
@@ -25,27 +64,59 @@ local function rand_admn()
   local aidx = math.random(1, nA)
   return admins[aidx]
 end
-local function rand_vrnt(test_type)
-  local min_num_variants = 2
-  local max_num_variants = 4
-  local nV = math.random(min_num_variants, max_num_variants)
+local function rand_vrnt(
+  TestType, 
+  NumVariants
+  )
+  assert(TestType)
+  assert(NumVariants)
   local V = {}
-  for i = 1, nV do 
+  local lp = "localhost:8080/AB/test_webapp/landing_page_"
+  local P = rand_perc(TestType, NumVariants)
+  for i = 1, NumVariants do 
     v = {}
-    if ( ( test_type == "ABTest" ) and ( i == 1 ) ) then 
+    if ( ( TestType == "ABTest" ) and ( i == 1 ) ) then 
       v.name = "Control"
     else
-      v.name = tostring(math.random(0, 1000000000))
+      v.name = rand_name()
     end
+    v.percentage = P[i]
+    if ( TestType == "XYTest" ) then 
+      v.url = lp .. tostring(i) .. ".php"
+    end
+    V[i] = v
+  end
+  return V
+end
 
-    v.name = "0
-
-  
+local function rand_test(X)
+  if ( not X ) then X = {} end 
+  local T = {}
+  T.name = X.name or rand_name()
+  T.Channel = X.Channel or rand_chnl()
+  T.Creator = X.Creator or rand_admn()
+  T.description = X.description or rand_dscr()
+  T.TestType = X.TestType or rand_type()
+  if ( T.TestType == "ABTest" ) then
+    T.is_dev_specific = false
+  else
+    local x = math.random(1, 2)
+    if ( x == 1 ) then T.is_dev_specific = true 
+    elseif ( x == 2 ) then T.is_dev_specific = false 
+    else assert(nil) end
+    
+  end
+  T.NumVariants = X.NumVariants or math.random(2, 8)
+  T.Variants = rand_vrnt(T.TestType, T.NumVariants)
+  print("XXXXXXXXXXXXXXXXXXX")
+  return T
 end
 
 R.rand_name = rand_name
 R.rand_chnl = rand_chnl
 R.rand_dscr = rand_dscr
 R.rand_admn = rand_admn
-R.rand_dev_specific = rand_dev_specific
+R.rand_test = rand_test
+R.rand_perc = rand_perc
+R.rand_dvsp = rand_dvsp
 return R
