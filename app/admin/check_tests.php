bce2acd47fe7338a7b49a984162859792538219a
Ramesh Subramonian <subramonian@gmail.com>
2018-07-20 09:25:54 +0000

    bug fixes and feature modifications

--- app/admin/check_tests.php
+++ app/admin/check_tests.php
@@ -1,12 +1,12 @@
-<?php session_start();
+<?php
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../php/db_helpers/");
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../php/");
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../php/helpers/");
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../php/rts/");
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../php/db_helpers/");
-require_once "includes/header_admin.php"; 
-require_once "chk_tests.php";
-require_once "chk_test.php"
+require_once "chk_tests.php"; 
+$rslt = chk_tests();
+require_once "includes/header_admin.php";
 ?>
 <div class="container">
 	<div class="row" >
@@ -16,17 +16,7 @@ require_once "chk_test.php"
               <h3 class="panel-title">Check Tests</h3>
             </div>
             <div class="panel-body">
-<?php
-/*$X = array();
-$X['TestName'] = 'Test1';
-$X['TestType'] = 'XYTest';
-$str_inJ = json_encode($X);
-$data = chk_test($str_inJ);
-print("<pre>".print_r($data,true)."</pre>");*/
-$rslt = chk_tests();
-print("<pre>".print_r($rslt,true)."</pre>");
-
-?>
+							<?php print("<pre>".print_r($rslt,true)."</pre>"); ?>
             </div></div>
           </div>
         </div></div>
