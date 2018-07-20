73f32cc791576d3112d8ef9650110b70e40033fa
Ramesh Subramonian <subramonian@gmail.com>
2018-07-20 05:11:29 +0000

    TestURL  working code implementation

--- app/aev_test_1.php
+++ app/aev_test_1.php
@@ -44,14 +44,15 @@ $config = config_html($TestType);
 
   <table class="table table-striped table-condensed" style="space=5px">
   <tbody>
-  <form class="form-signin" id='addTest' method='POST'>
+<form  id='TestURL' class="form-signin" method="POST"></form>
+  <form class="form-signin" id='addTest' method='POST'></form>
   <!-- DISPLAY LOGIC FOR TEST ID & TEST NAME START -->
   <?php if(($mode == "Edit") || ($mode == "View")) { ?>
 	<tr>
 		<td colspan="3">Test ID: <?php echo $id; ?></td>
 	</tr>
 	<tr>
-		<td colspan="3">Test Name: <?php echo $TestName; ?><input type='hidden' name='TestName' value='<?php echo $TestName; ?>'></td>
+		<td colspan="3">Test Name: <?php echo $TestName; ?><input form="addTest" type='hidden' name='TestName' value='<?php echo $TestName; ?>'></td>
 	</tr>
   <?php if ( isset($TestType) && ($TestType == "XYTest")) { ?>
   <tr>
@@ -60,7 +61,7 @@ $config = config_html($TestType);
   </tr>
   <?php  if ($this_state == "started") { ?>
   <tr>
-  <td>Device: <form method="POST" id='TestURL' class="form-signin"><input type="text" name="device"></form></td><td ><input class="btn btn-sm btn-primary btn-block" type="submit" form="TestURL" id="test_url" value="Test URL"></td><td>&nbsp;</td>
+  <td>Device: <input form="TestURL" type='hidden' name='nw_campaign_id' value='<?php echo $external_id; ?>'><input form='TestURL' type="text" name="device" ></td><td ><input class="btn btn-sm btn-primary btn-block" type="submit" form='TestURL' id="test_url" value="Test URL"></td><td>&nbsp;</td>
   </tr>
   <?php } 
           } 
@@ -70,7 +71,7 @@ $config = config_html($TestType);
   <td colspan="3">Test Name &nbsp; 
     <span class="glyphicon glyphicon-question-sign" data-placement="top" data-toggle="tooltip" href="#" data-original-title="Provide a descriptive name for your test that represents your vertical, date, and test. It should be easy to read and memorable. .Only Alphanumeric char without space">
     </span>
-  <input type="text" name="TestName" size="<?php echo $config['name_size']; ?>" maxlength="<?php echo $config['name_maxlength']; ?>" pattern="^[A-Za-z0-9\S]{1,31}$" required>
+  <input form="addTest" type="text" name="TestName" size="<?php echo $config['name_size']; ?>" maxlength="<?php echo $config['name_maxlength']; ?>" pattern="^[A-Za-z0-9\S]{1,31}$" required>
 
   </td>
   </tr>
@@ -82,7 +83,7 @@ $config = config_html($TestType);
   <!-- DISPLAY LOGIC FOR TEST ID & TEST NAME END -->
   <tr> 
   <td colspan="3">Description &nbsp;<span class="glyphicon glyphicon-question-sign" data-placement="top" data-toggle="tooltip" href="#" data-original-title="Provide a friendly description for what the test is for and what it is trying to validate. Please include a wiki link. "></span>
-  <textarea class="form-control" rows="3" cols="20" name="TestDescription" 
+  <textarea form="addTest" class="form-control" rows="3" cols="20" name="TestDescription" 
     maxlength="255" 
     <?php echo $readonly; ?> required>
   <?php if (isset($description)) {echo $description;}?>
@@ -101,7 +102,7 @@ if ( $mode == "View" )
 else
   { 
 ?>
-  <select name='Channel'>";
+  <select form="addTest" name='Channel'>";
   <option value=''>None</option>
 <?php 
 $channel    = db_get_rows('channel');
@@ -119,7 +120,7 @@ for ( $i = 0; $i < $nC; $i++ ) {
 if ( isset($TestType) && ($TestType == "XYTest")) {
   if ( $mode == "Edit" ) {
     if ($this_state == "started") { ?>
-Change URL(s)?:&nbsp;&nbsp;<input type="checkbox" data-toggle="modal" data-target="#ConfirmationModal" name="modify_url" value="1" >
+Change URL(s)?:&nbsp;&nbsp;<input form="addTest" type="checkbox" data-toggle="modal" data-target="#ConfirmationModal" name="modify_url" value="1" >
 <?php 
     } 
   } 
@@ -132,12 +133,12 @@ Change URL(s)?:&nbsp;&nbsp;<input type="checkbox" data-toggle="modal" data-targe
   $max_prop = (100 /($num_var));
   ?>
   <tr> 
-   <input type='hidden' name='VID_<?php echo $i; ?>' value='<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['id']; } ?>'>
+   <input form="addTest" type='hidden' name='VID_<?php echo $i; ?>' value='<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['id']; } ?>'>
     <td>Variant <?php echo $i + 1; ?>&nbsp;:<?php if ($mode == "Edit") {echo $rslt['Variants'][$i]['id'];} ?>&nbsp;&nbsp;<span class='glyphicon glyphicon-question-sign' data-placement='top' data-toggle='tooltip' href='#' data-original-title=' Code-readable name for this variant as used by engineering. Should be descriptive with no spaces or special characters, i.e. apply_now_blue. Only Alphanumeric char without space'></span>
-  <input type='text' size='16' name='VName_<?php echo $i; ?>' maxlength='15' pattern='^[A-Za-z0-9\S]{1,15}$' 
+  <input form="addTest" type='text' size='16' name='VName_<?php echo $i; ?>' maxlength='15' pattern='^[A-Za-z0-9\S]{1,15}$' 
 value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['name']; } ?>" <?php echo $readonly; ?> required></td>
   <td>Landing Page URL &nbsp;<span class='glyphicon glyphicon-question-sign' data-placement='top' data-toggle='tooltip' href='#' data-original-title='Absolute URL of the landing page for this variant.'></span>
-  <input type='url' class='btn btn-default'  name='VURL_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['url'];} ?>" id="url_<?php echo $i; ?>" 
+  <input form="addTest" type='url' class='btn btn-default'  name='VURL_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['url'];} ?>" id="url_<?php echo $i; ?>" 
 <?php 
 if (($mode == "Edit") && ($this_state == "started")) { 
   echo "readonly"; 
@@ -147,18 +148,18 @@ if (($mode == "Edit") && ($this_state == "started")) {
 ?> required>
   </td>
   <td>Distribution:&nbsp;&nbsp; 
-  <input type='text' style='width:5em'  size='3' name='VPercentage_<?php echo $i; ?>' class='prop' 
+  <input form="addTest" type='text' style='width:5em'  size='3' name='VPercentage_<?php echo $i; ?>' class='prop' 
 value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['percentage'];} ?>"required <?php echo $readonly; ?>></td></tr>
 <?php } }elseif ($TestType == "ABTest") { ?>
   <tr>
 <input type='hidden' name='VID_0' value='<?php if ($mode != "Add") {echo $rslt['Variants'][0]['id']; } ?>'>
   <td>Original Feature&nbsp;<span class="glyphicon glyphicon-question-sign" data-placement="top" data-toggle="tooltip" href="#" data-original-title="Standard Feature is called Control"></span>
-  <input type="text" name="VName_0" size="16" maxlength="15" value="Control" readonly="readonly"></td>
+  <input form="addTest" type="text" name="VName_0" size="16" maxlength="15" value="Control" readonly="readonly"></td>
   <!--<td>Description
   <textarea class="form-control" rows="3" cols="9" maxlength="128"  readonly="readonly" >Standard Feature</textarea></td>-->
   <td></td>
   <td>Percentage &nbsp;&nbsp;
-  <input type="number"  style='width:5em' min="0" max="100" maxlength="3" size='2' id="control"  class="control" name="VPercentage_0" value="<?php echo $rslt['Variants'][0]['percentage']; ?>" readonly="readonly"></td>
+  <input form="addTest" type="number"  style='width:5em' min="0" max="100" maxlength="3" size='2' id="control"  class="control" name="VPercentage_0" value="<?php echo $rslt['Variants'][0]['percentage']; ?>" readonly="readonly"></td>
   </tr>
 <?php 
   for ( $i = 1; $i <= $num_var; $i++ ) { 
@@ -167,27 +168,27 @@ value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['percentage'];} ?>"
   <tr>
    <input type='hidden' name='<?php echo "VID_".$i; ?>' value='<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['id']; } ?>'>
   <td>Variant <?php echo $i + 1; ?>&nbsp;&nbsp;( ID : <?php if ($mode == "Edit") {echo $rslt['Variants'][$i]['id'];} ?>&nbsp;)&nbsp;<span class='glyphicon glyphicon-question-sign' data-placement='top' data-toggle='tooltip' href='#' data-original-title=' Code-readable name for this variant as used by engineering. Should be descriptive with no spaces or special characters, i.e. apply_now_blue. Only Alphanumeric char without space'></span>
-  <input type='text' size='16' name='VName_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['name']; } ?>" maxlength='15' pattern='^[A-Za-z0-9\S]{1,15}$' required></td>
+  <input form="addTest" type='text' size='16' name='VName_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['name']; } ?>" maxlength='15' pattern='^[A-Za-z0-9\S]{1,15}$' required></td>
   <!--<td>Description &nbsp;<span class='glyphicon glyphicon-question-sign' data-placement='top' data-toggle='tooltip' href='#' data-original-title=' Human-readable description for what this variant is, i.e. `the blue apply now button link.`'></span>-->
 <!--
   <textarea class='form-control' rows='3' cols='9' maxlength='128' name='var".$i."_desc' required></textarea>
 -->
 </td>
 <td></td>
-  <td>Percentage &nbsp;&nbsp;<input type='number' style='width:5em' min='0' max='<?php echo (100/$num_var); ?>' step='1' size='2' name='VPercentage_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['percentage'];} ?>" class='prop' required></td>
+  <td>Percentage &nbsp;&nbsp;<input form="addTest" type='number' style='width:5em' min='0' max='<?php echo (100/$num_var); ?>' step='1' size='2' name='VPercentage_<?php echo $i; ?>' value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['percentage'];} ?>" class='prop' required></td>
 <?php } ?>
   </tr>
 <?php } ?>
-<input type='hidden' name='TestID' value='<?php echo $id; ?>'>
-<input type='hidden' name='NumVariants' value="<?php if (isset($n_var)) {echo $n_var; }?>">
+<input form="addTest" type='hidden' name='TestID' value='<?php echo $id; ?>'>
+<input form="addTest" type='hidden' name='NumVariants' value="<?php if (isset($n_var)) {echo $n_var; }?>">
 <?php if ($mode == "Add") { ?>
-<input type='hidden' name='Creator' value='<?php echo $User; ?>'>
+<input form="addTest" type='hidden' name='Creator' value='<?php echo $User; ?>'>
 <?php } else { ?>
-<input type='hidden' name='Updater' value='<?php echo $User; ?>'>
-<input type='hidden' name='State' value='<?php echo $this_state; ?>'>
+<input form="addTest"type='hidden' name='Updater' value='<?php echo $User; ?>'>
+<input form="addTest" type='hidden' name='State' value='<?php echo $this_state; ?>'>
 <?php } ?>
-<input type='hidden' name='TestType' value='<?php echo $TestType; ?>'>
-<input type='hidden' name='BinType' value='<?php if (isset($BinType)) { echo $BinType;} ?>'>
+<input form="addTest" type='hidden' name='TestType' value='<?php echo $TestType; ?>'>
+<input form="addTest" type='hidden' name='BinType' value='<?php if (isset($BinType)) { echo $BinType;} ?>'>
   <tr>
     <td></td>
     <td >TOTAL: &nbsp; &nbsp;
@@ -196,7 +197,6 @@ value="<?php if ($mode != "Add") {echo $rslt['Variants'][$i]['percentage'];} ?>"
     </td>
     <td> </td>
   </tr>
-</form>
 <tr>
 <td><a href="home.php"><button class="btn btn-lg btn-primary btn-block" >Cancel</button></a></td>
 <?php 
