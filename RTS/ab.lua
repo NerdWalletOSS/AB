97bdb1727a46565c893fb5522395c49e10b90476
Ramesh Subramonian <subramonian@gmail.com>
2018-07-21 11:28:41 -0700

    ok

--- RTS/ab.lua
+++ RTS/ab.lua
@@ -12,10 +12,16 @@ function update_config(...)
   x_update_config(...)
 end
 
-
+--TODO P3 Is this x_get_test hackery needed?
 local x_get_test      = require 'RTS/get_test'
 function get_test(...)
   x, y =  x_get_test(...)
   return x, y
+end
+
+local x_update_test      = require 'RTS/update_test'
+function update_test(...)
+  x =  x_update_test(...)
+  return x
 end
 
---
+++ RTS/update_test.lua
@@ -0,0 +1,10 @@
+local assertx    = require 'lua/assertx'
+local ffi        = require 'lua/ab_ffi'
+local consts     = require 'lua/ab_consts'
+local JSON       = require 'lua/JSON'
+
+local function update_test(
+  )
+  return true
+end
+return update_test
