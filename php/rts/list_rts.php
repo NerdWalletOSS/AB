fcf627f333c75d57d55dd08c573c204b391819d3
Evan Borgstrom <eborgstrom@nerdwallet.com>
2018-07-25 14:25:59 -0700

    Initial ECS support

--- php/rts/list_rts.php
+++ php/rts/list_rts.php
@@ -6,6 +6,9 @@ set_include_path(get_include_path() . PATH_SEPARATOR . "../");
 require_once "rs_assert.php";
 require_once "load_configs.php";
 require_once "get_url.php";
+require_once 'aws.phar';
+
+use Aws\Ecs\EcsClient;
 
 function list_rts()
 {
@@ -13,9 +16,36 @@ function list_rts()
 
   switch ($config->{'AB'}->{'RTS_FINDER'}->{'METHOD'}->{'VALUE'}) {
   case "ecs":
-    break;
+    $client = EcsClient::factory(array(
+      'profile' => 'nwdev',
+      'region'  => 'us-east-1',
+      'version' => '2014-11-13',
+    ));
+    $result = $client->listTasks([
+      'desiredStatus' => 'RUNNING',
+      'cluster' => $config->{'AB'}->{'RTS_FINDER'}->{'ECS_CLUSTER'}->{'VALUE'},
+      'family' => $config->{'AB'}->{'RTS_FINDER'}->{'ECS_FAMILY'}->{'VALUE'},
+    ]);
+
+    $result = $client->describeTasks([
+      'cluster' => $config->{'AB'}->{'RTS_FINDER'}->{'ECS_CLUSTER'}->{'VALUE'},
+      'tasks' => $result['taskArns'],
+    ]);
+
+    $ret = array();
+    foreach ($result['tasks'] as $task) {
+      foreach ($task['containers'] as $container) {
+        $ret[] = array(
+          'server' => $container['networkBindings'][0]['bindIP'],
+          'port' => $container['networkBindings'][0]['hostPort'],
+        );
+      }
+    }
+    return $ret;
 
   case "local":
+    // our config comes from loaded JSON, which is all stdObject
+    // the code that consumes it is built to work with arrays, so this transforms our objects into arrays
     $ret = array();
     foreach ($config->{'AB'}->{'RTS_FINDER'}->{'SERVERS'}->{'VALUE'} as $server) {
       $ret[] = (array) $server;
@@ -24,5 +54,6 @@ function list_rts()
 
   default:
     echo("Unknown RTS finder method");
+    die;
   }
 }
