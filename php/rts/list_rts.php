49b406cb59b7bdfc112bb45a14743b13a79d6188
Evan Borgstrom <eborgstrom@nerdwallet.com>
2018-07-25 13:59:29 -0700

    Cleanup configuration and introduce ECS RTS discovery

--- php/rts/list_rts.php
+++ php/rts/list_rts.php
@@ -7,71 +7,22 @@ require_once "rs_assert.php";
 require_once "load_configs.php";
 require_once "get_url.php";
 
-function list_rts(
-)
+function list_rts()
 {
-  $servers = null;
-  $ports   = null;
-  // Load configs 
-  rs_assert(load_configs());
-  $conf_file = "/opt/abadmin/db2.json";
-  rs_assert(is_file($conf_file), "File not found $conf_file");
-  $configs = json_decode(file_get_contents($conf_file));
-  rs_assert($configs, "unable to JSON decode $conf_file");
-  //-----------------------------------------------------------
-  //--- Original case: just one server
-  if ( $configs->{'rts_finder_server'} == "" ) {
-    $server = $configs->{'ab_rts_server'};
-    $port   =  $configs->{'ab_rts_port'};
-    if ( ( $server == "" ) || ($port == "" ) )  {
-      echo("NO RTS found\n");
-      return null;
-    }
+  $config = read_config_file();
 
-    $SP = array(1);
-    $SP[0] = array('server' => $server, 'port' => $port);
-    if ( getenv("AB_WEBAPP_STANDALONE") == "true" ) {
-      $SP = null;
+  switch ($config->{'AB'}->{'RTS_FINDER'}->{'METHOD'}->{'VALUE'}) {
+  case "ecs":
+    break;
+
+  case "local":
+    $ret = array();
+    foreach ($config->{'AB'}->{'RTS_FINDER'}->{'SERVERS'}->{'VALUE'} as $server) {
+      $ret[] = (array) $server;
     }
-    return $SP;
-  }
-  //-----------------------------------------------------------
-  if ( ( $configs->{'rts_finder_server'} != "" ) && 
-       ( $configs->{'rts_finder_port'} != "" ) )  {
-    // do something 
-    $s = $configs->{'rts_finder_server'} ;
-    $p = $configs->{'rts_finder_port'} ;
-    $http_code = 0; $rslt = "";
-    get_url($s, $p, "DescribeInstances", $http_code, $rslt);
-    if ( $http_code != 200 ) { return null; }
-    $X = json_decode($rslt);
-    if ( !$X ) { return null; }
-    if ( !isset($X->{'server'}) ) { return null; } if ( !isset($X->{'port'}) ) { return null; }
-    $servers = $X->{'server'};
-    $ports = $X->{'port'};
-    if ( count($servers) != count($ports) )  { return null; }
-    $n = count($servers);
-    $SP = array($n);
-    for ( $i = 0; $i < $n; $i++ ) { 
-      $SP[$i] = array('server' => $servers[$i], 'port' => $ports[$i]);
-    }
-    return $SP;
-  }
-  else {
-    echo("NO RTS found\n");
-    return null;
+    return $ret;
+
+  default:
+    echo("Unknown RTS finder method");
   }
 }
-/*
-$SP = list_rts();
-if ( $SP ) { 
-   foreach ( $SP as $sp ) { 
-     $server = $sp['server']; $port = $sp['port'];
-     echo "server = $server, port = $port \n";
-  }
-}
-else {
-  echo "Not using RTS\n";
-}
- */
-?>
