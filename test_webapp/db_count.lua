2ea678c515b6d50d2b972d687a6e22c984f7f3d5
Ramesh Subramonian <subramonian@gmail.com>
2018-07-31 11:49:46 -0700

    ok

--- test_webapp/db_count.lua
+++ test_webapp/db_count.lua
@@ -2,13 +2,20 @@ local get_db_config = require 'test_webapp/get_db_config'
 local db_connect    = require 'RTS/db_connect'
 
 local function db_count(
-  tbl
+  tbl,
+  where
   )
   assert(type(tbl) == "string")
 
   local db, host, user, pass, port = get_db_config()
   local conn = db_connect(db, host, user, pass, port)
-  local T = conn:query('select count(*) from ' .. tbl)
+  local query_str = 'select count(*) from ' .. tbl
+  if ( where ) then 
+    assert(type(where) == "string")
+    query_str = query_str .. " " .. where_str 
+  end
+
+  local T = conn:query(query_str)
   assert(type(T) == "table")
   assert(type(T[1]) == "table")
   local nR = T[1]["count(*)"]
