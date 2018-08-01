1570c1a276b8f792b31519b17af28af3b095c4d2
Ramesh Subramonian <subramonian@gmail.com>
2018-08-01 18:54:46 +0000

    showing response for TestURL

--- app/js/insert_test.js
+++ app/js/insert_test.js
@@ -49,25 +49,26 @@ $(document).ready(function() {
       url: "processor/test_url_processor.php",
       data: $(this).serialize(),
       error: function(response, textStatus, XHR) {
+        console.log(response);
         if (response.getResponseHeader('Error-Code') != 200) {
           var cssLink = "css/error.css";
           $("head").append("<link href=" + cssLink + " rel='stylesheet' />");
           $("#error").css('display', 'inline', 'important');
           $("#error_message").css('display', 'inline', 'important');
           $("#stack_trace").css('display', 'inline', 'important');
-          $("#error_message").html(response.getResponseHeader('Error-Message'));
+          $("#error_message").html(response);
           $("#stack_trace").html(response.getResponseHeader('Error-BackTrace'));
         }
       },
       success: function(response, textStatus, XHR) {
-        //var id = XHR.getResponseHeader('TestID');
-        //window.location = "aev_test_1.php";
+        console.log(response);
           var cssLink = "css/error.css";
           $("head").append("<link href=" + cssLink + " rel='stylesheet' />");
           $("#error").css('display', 'inline', 'important');
           $("#error_message").css('display', 'inline', 'important');
           $("#stack_trace").css('display', 'inline', 'important');
-          $("#error_message").html(XHR.getResponseHeader('URLReturned'));
+          //$("#error_message").html(XHR.getResponseHeader('URLReturned'));
+					$("#error_message").html(response);
       },
       beforeSend: function() {
         $("#error_message").css('display', 'inline', 'important');
