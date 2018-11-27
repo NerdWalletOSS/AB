161f564d01c6d7ea0c3a4cc8f741f9ce94aceb7e
krushna <krushnakant.mardikar@gslab.com>
2018-11-27 03:07:13 -0800

    Removed the hardcoding of C function while performing ffi.cdef, using header files to get funciton signature

--- lupa/init.lua
+++ lupa/init.lua
@@ -3,6 +3,8 @@ local ffi = require 'ffi'
 lupa_dt = ffi.load('liblupa_dt')
 require 'DT/init'
 local load_model = require 'DT/load_model'
+local cdef_list = require 'lupa/cdef_list'
+
 local function init(model_dir, forest_type)
   conf = {} -- this is a global
   conf.DECISION_TREE_DIRECTORY = model_dir
@@ -16,32 +18,19 @@ local function init(model_dir, forest_type)
   assert(load_model(model_dir))
   --  cdef necessary structures
   local tbl = {}
-  for line in io.lines('../dt_interpreter/src/dt_types.h') do
-    local fc = string.sub(line, 1, 1)
-    if ( fc ~= '#' ) then 
-      tbl[#tbl+1] = line
+  for _, h_file in ipairs(cdef_list) do
+    for line in io.lines(h_file) do
+      local fc = string.sub(line, 1, 1)
+      if ( fc ~= '#' ) then 
+        tbl[#tbl+1] = line
+      end
     end
   end
+
   tbl[#tbl+1] = "void * malloc(size_t size); "
   tbl[#tbl+1] = " void *memset(void *s, int c, size_t n);"
   tbl[#tbl+1] = " void free(void *ptr); "
   ffi.cdef(table.concat(tbl, "\n"))
-  -- now cdef all the C functions you will need
-  -- TODO FOllowng hard coding should be avooided 
-  ffi.cdef([[
-  extern int load_models(
-    const char * const model_dir,
-    const char * const forest_type,
-    int num_features,
-    DT_INTERPRETER_TYPE *interp
-    );
-  extern void free_interp(
-    DT_INTERPRETER_TYPE *interp
-    );
-  extern int lua_eval_mdl(
-    DT_INTERPRETER_TYPE *interp
-    );
-  ]])
   --===========================================
   -- g_interp is a global
   -- TODO use ffi.gc to set automatic free
